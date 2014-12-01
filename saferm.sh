8b47be2dafe2c9816b695d9ae5f340fc5024b9db
jctudela <jtudela@faculty.ocad.ca>
2014-12-01 11:49:47 -0500

    Create saferm.sh
    This will clean /Users only removing user folders named in the form of letter-letter-number-number-letter-letter, when the user is not logged in, and if the home folder is older than 45 minutes

---
+++ saferm.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Define current user
+current_user=`ls -l /dev/console | awk '{print $3}'`
+
+# Loop through users with homes in /Users;
+# exclude any accounts you don't want removed
+# (i.e. local admin and current user if script runs
+# while someone is logged in)
+
+for dir in `find /Users/* -mtime +45m -type d -name [a-z][a-z][0-9][0-9][a-z][a-z] | grep -v '$current_user'`
+do
+ echo "Removing user: $dir"
+# dscl . delete $dir
+# rm -rf $dir
+done
+# End of script
