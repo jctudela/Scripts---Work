8c6bea7bb1bc4e05616126b770a3321e8c86e7e1
jctudela <jtudela@faculty.ocad.ca>
2015-01-26 16:29:25 -0500

    Create InstallAllRhino.bat

---
+++ InstallAllRhino.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+msiexec /i C:\ProgramData\LANrev\InstTmp\F8F63AD5-1E8D-4DD4-A695-EDC4B2272F0E_\rhino5setup_en-us_x64.msi /qb
+msiexec /i C:\ProgramData\LANrev\InstTmp\F8F63AD5-1E8D-4DD4-A695-EDC4B2272F0E_LanguagePack_en-us.msi /qb 
+msiexec /i C:\ProgramData\LANrev\InstTmp\F8F63AD5-1E8D-4DD4-A695-EDC4B2272F0E_HelpMedia.msi /qb
