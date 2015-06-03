31ceebb7e17fb7c2eb506f1df8f2cc34632726c9
jctudela <jtudela@faculty.ocad.ca>
2015-06-03 08:32:48 -0400

    Update customizeagent.sh

--- osx/absolute-manage/customizeagent.sh
+++ osx/absolute-manage/customizeagent.sh
@@ -6,10 +6,13 @@
 defaults write /Library/Preferences/com.poleposition-sw.lanrev_agent.plist AbsoluteRemoteEnabled true
 
 #Specify password for remote
-defaults write /Library/Preferences/com.poleposition-sw.lanrev_agent.plist AbsoluteRemotePassword ***ENTER HASH HERE***
+defaults write /Library/Preferences/com.poleposition-sw.lanrev_agent.plist AbsoluteRemotePassword 686D64AACB64A3CC8775639F308742BBB3C479B42CCFA14B
 
 #Specify port for remote
 defaults write /Library/Preferences/com.poleposition-sw.lanrev_agent.plist AbsoluteRemotePort 5901
+
+#No user confirmation required for remote
+defaults write /Library/Preferences/com.poleposition-sw.lanrev_agent.plist AbsoluteRemoteUserConfirmationRequired false
 
 #Check for Apple Software Updates
 defaults write /Library/Preferences/com.poleposition-sw.lanrev_agent.plist CheckAppleSoftwarePatches true
